0150568fc2e1ceb7008cbd362d36ccbd375e5364
RanetaCroak <frogspprt@gmail.com>
2022-11-15 02:30:54 +0100

    Create ej3.java
    Principio del ejercicio

---
+++ Java/Bucles/ej3.java
@@ -0,0 +1,14 @@
+package Java.Bucles;
+
+import java.util.Scanner;
+
+public class ej3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vamos a jugar al número secreto");
+        System.out.println("Introduce números y te indicaré \"mayor\" o \"menor\" hasta que aciertes");
+        System.out.println("Para rendirte, introduce un -1");
+        
+    }
+}
